132e54a6b18f0e4996219e2e95be282055e2bb81
Adam Marshall <adam@appsynergy.net>
2016-03-29 18:23:26 +0100

    Oops, here's the plugin

--- assets/profiles/disable.php
+++ assets/profiles/disable.php
@@ -1,34 +1,39 @@
 <?php
 /*
 Plugin Name: Restrict User Editing Own Profile
-Plugin URI: http://www.philosophydesign.com
+Plugin URI: http://www.appsynergy.net
 Description: Restricts users from editing their own profile information.
-Author: Scott Cariss
+Author: Adam Marshall & Scott Cariss
 Version: 0.2
 Author URI: http://www.philosophydesign.com/scott-cariss.html
 */
 
-add_action( 'admin_menu', 'stop_access_profile' );
-add_action('after_setup_theme', 'remove_admin_bar');
 
-function remove_admin_bar() {
-if (!current_user_can('administrator') && !is_admin()) {
-  show_admin_bar(false);
-}
-}
-function stop_access_profile() {
+add_action('after_setup_theme', 'profiles_disable_admin_bar');
+add_action('admin_menu', 'profiles_disable_menu' );
+add_action('admin_menu', 'profiles_disable_die' );
 
-  //
+
+function profiles_disable_admin_bar() {
   if (!current_user_can('administrator') && !is_admin()) {
     show_admin_bar(false);
   }
+}
 
 
-  remove_menu_page( 'profile.php' );
-  remove_submenu_page( 'users.php', 'profile.php' );
+function profiles_disable_menu() {
+  remove_menu_page('profile.php');
+  remove_submenu_page('users.php', 'profile.php');
+}
 
-  if(defined('IS_PROFILE_PAGE') && IS_PROFILE_PAGE === true) {
-      wp_die( 'You are not permitted to change your own profile information. Please contact us if you think profile information should be changed.' );
+
+function profiles_disable_die() {
+  if (!current_user_can('administrator') && !is_admin()) {
+    if(defined('IS_PROFILE_PAGE') && IS_PROFILE_PAGE === true) {
+        wp_die('You are not permitted to change your own profile information. Please contact us if you think profile information should be changed.');
+    }
   }
 }
+
+
 ?>
