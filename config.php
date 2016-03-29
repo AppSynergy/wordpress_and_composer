f0993be78c7f873060b25d1b4d9a17110f440d9e
Adam Marshall <adam@appsynergy.net>
2016-03-29 19:06:06 +0100

    Add DOTENV. I know .env should be ignored, but it's also the example for now

--- config.php
+++ config.php
@@ -1,26 +1,31 @@
 <?php
 
+// ENVIROMENT VARIABLES
+require __DIR__ . '/vendor/autoload.php';
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
 // DATABASE
-$table_prefix = 'tbl_';
-define('DB_NAME', 'composewp');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+$table_prefix = $_ENV['DB_PREFIX'];
+define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_HOST', $_ENV['DB_HOST']);
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
 // SALTING
-define('AUTH_KEY', '&cWFU66|Onl$+@[qc!h=3(dsW/RCco+,b.^bBnxr^%|*?fs{x~4LR1GF0DYu:=,C');
-define('SECURE_AUTH_KEY', '%!>.s2MQUJX{F[nO&*Sin[?n6K%X|>:qW+aIiDe4h9^>6Q;IP#o1P}LgON-HAV `');
-define('LOGGED_IN_KEY', 'Jk&o`cq}y-Xg~G^B!2f)J8,x,Di.uQ$5JBokZ<%(IIFx<,Z?o+R>4AEYx R{Y|BA');
-define('NONCE_KEY', '69Ew <D*1~v3r-b*Ob-y7}!@Mp7h$+KE2AR+.!`vDtzciiUeAI|3b-bW?QN+94jr');
-define('AUTH_SALT', 'm@F7Y=bpJF:*a`&Bx|nJXi*1bQ69I(-!pwPuPqL`=(n.?pi2&^zy[;>7FH t-mWw');
-define('SECURE_AUTH_SALT', 's`-5FbO5GO(^W[,+,lJ)WP&K$-=]^0Q}|-&;;f>y%r2|jw/(8j[yi^U/FRGpAKh@');
-define('LOGGED_IN_SALT', 'Q/!k&2]xD,s2jzgBM+3/xt8_++b%Z~t^K2<iLmg=IK.|:+D{dM0kWt~`!#)>CL+q');
-define('NONCE_SALT', '2|& db=$9+}55JNUEQ?j,6)ek~9YTpRd!|!3GeK=Z&FS70HgzMUn56Rv9T$|Uc6i');
+define('AUTH_KEY', $_ENV['KEY_1']);
+define('SECURE_AUTH_KEY', $_ENV['KEY_2']);
+define('LOGGED_IN_KEY', $_ENV['KEY_3']);
+define('NONCE_KEY', $_ENV['KEY_4']);
+define('AUTH_SALT', $_ENV['KEY_5']);
+define('SECURE_AUTH_SALT', $_ENV['KEY_6']);
+define('LOGGED_IN_SALT', $_ENV['KEY_7']);
+define('NONCE_SALT', $_ENV['KEY_8']);
 
 // ARCHITECTURE
-define('WP_HOME', 'http://localhost/Dev/composewp/public');
+define('WP_HOME', $_ENV['HOME']);
 define('WP_SITEURL', WP_HOME.'/wp');
 define('WP_CONTENT_DIR', dirname(__FILE__).'/public/content');
 define('WP_CONTENT_URL', WP_HOME.'/content');
