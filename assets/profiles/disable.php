493261529ec7f27b9dc28eaca4f754ad5bfcbcbc
Adam Marshall <adam@appsynergy.net>
2016-03-29 18:22:13 +0100

    Good a time as any for the profile r&d

--- assets/profiles/disable.php
+++ assets/profiles/disable.php
@@ -4,16 +4,31 @@ Plugin Name: Restrict User Editing Own Profile
 Plugin URI: http://www.philosophydesign.com
 Description: Restricts users from editing their own profile information.
 Author: Scott Cariss
-Version: 0.1
+Version: 0.2
 Author URI: http://www.philosophydesign.com/scott-cariss.html
 */
 
 add_action( 'admin_menu', 'stop_access_profile' );
+add_action('after_setup_theme', 'remove_admin_bar');
+
+function remove_admin_bar() {
+if (!current_user_can('administrator') && !is_admin()) {
+  show_admin_bar(false);
+}
+}
 function stop_access_profile() {
-    //remove_menu_page( 'profile.php' );
-    //remove_submenu_page( 'users.php', 'profile.php' );
-    if(IS_PROFILE_PAGE === true) {
-        wp_die( 'You are not permitted to change your own profile information. Please contact us if you think profile information should be changed.' );
-    }
+
+  //
+  if (!current_user_can('administrator') && !is_admin()) {
+    show_admin_bar(false);
+  }
+
+
+  remove_menu_page( 'profile.php' );
+  remove_submenu_page( 'users.php', 'profile.php' );
+
+  if(defined('IS_PROFILE_PAGE') && IS_PROFILE_PAGE === true) {
+      wp_die( 'You are not permitted to change your own profile information. Please contact us if you think profile information should be changed.' );
+  }
 }
 ?>
